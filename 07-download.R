6eaaed017c817354967be8603f8257662f42978c
Kirill Voronin <yoursdearboy@gmail.com>
2025-04-01 15:11:07 +0300

    Заменил датасет на IMDB

--- 07-download.R
+++ 07-download.R
@@ -1,15 +1,15 @@
 library(readr)
 library(dplyr)
 
-url <- "https://yoursdearboy.github.io/git-101/data/billboard100.csv"
+url <- "https://yoursdearboy.github.io/git-101/data/imdb250.csv"
 
-httr::GET(url, httr::write_disk("billboard.csv", overwrite = T))
+httr::GET(url, httr::write_disk("imdb250.csv", overwrite = T))
 
-top <- read_csv("billboard.csv")
+top <- read_csv("imdb250.csv")
 
 top10 <- top |>
-  group_by(Date) |>
+  group_by(year) |>
   filter(row_number() <= 10) |>
   ungroup()
 
-write_csv(top10, "billboard.csv")
+write_csv(top10, "imdb10.csv")
